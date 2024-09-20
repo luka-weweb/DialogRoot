f737d87ce0da86d24e764a9ae62395024b5e20c1
luka-weweb <luka@weweb.io>
2024-09-20 10:12:25 +0200

    fixes

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,13 +1,80 @@
 <template>
-  <wwElement v-bind="this.content.contentElement" />
+  <div>
+    <wwElement
+      v-if="this.content.trigger"
+      v-bind="this.content.elementTrigger"
+      @click="dialogOpen = !dialogOpen"
+    />
+    <wwElement
+      v-if="this.content.overlay"
+      v-bind="this.content.elementOverlay"
+      :teleport="this.content.teleport"
+      :escapeCloses="this.content.escapeCloses"
+      :open="this.isDialogOpen"
+      style="z-index: 1"
+    />
+    <wwElement
+      v-bind="this.content.elementContent"
+      :type="this.content.type"
+      :styleSettings="this.styleSettings"
+      :teleport="this.content.teleport"
+      :escapeCloses="this.content.escapeCloses"
+      :open="this.isDialogOpen"
+      style="z-index: 2; pointer-events: auto"
+      @keyup.escape="
+        this.content.escapeCloses ? (this.dialogOpen = false) : null
+      "
+    />
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     content: { type: Object, required: true },
+    uid: { type: String, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
+  },
+  data() {
+    return {
+      dialogOpen: false,
+    };
+  },
+  computed: {
+    styleSettings() {
+      switch (this.content.type) {
+        case "sheet":
+          return ["sheet", `-${this.content.sheetSide}`];
+          break;
+        case "modal":
+          return [
+            "modal",
+            `-align-${this.content.modalSide}`,
+            `-side-${this.content.modalAlignment}`,
+          ];
+      }
+    },
+    isDialogOpen() {
+      if (this.content.manualControl) {
+        return this.content.value;
+      } else {
+        return this.dialogOpen;
+      }
+    },
+  },
+  watch: {
+    isDialogOpen(newValue) {
+      if (newValue) {
+        const frontDocument = wwLib.getFrontDocument();
+        const appEl = frontDocument.querySelector("body");
+        if (appEl) {
+          appEl.style.pointerEvents = "none";
+        }
+      } else {
+        appEl.style.pointerEvents = "auto";
+      }
+    },
   },
 };
 </script>
--- ww-config.js
+++ ww-config.js
@@ -6,12 +6,164 @@ export default {
     icon: "view-grid",
   },
   properties: {
-    contentElement: {
+    elementTrigger: {
       hidden: true,
       defaultValue: {
-        isWwObject: false,
+        isWwObject: true,
+        type: "63912cf2-b4fe-4cf8-aa7e-e6f29bf4157f",
+      },
+    },
+    elementOverlay: {
+      hidden: true,
+      defaultValue: {
+        isWwObject: true,
+        type: "7572eedc-3d73-43e1-8d16-bf977b0c8e05",
+      },
+    },
+    elementContent: {
+      hidden: true,
+      defaultValue: {
+        isWwObject: true,
         type: "3049f8aa-e796-4a28-aafd-a282baab589e",
       },
+    },
+    type: {
+      label: {
+        en: "Type",
+      },
+      type: "TextSelect",
+      defaultValue: "dialog",
+      options: {
+        options: [
+          { value: "dialog", label: { en: "Dialog" } },
+          { value: "modal", label: { en: "Modal" } },
+          { value: "sheet", label: { en: "Sheet" } },
+          { value: "drawer", label: { en: "Drawer" } },
+          { value: "alert", label: { en: "Alert" } },
+        ],
+      },
+    },
+    sheetSide: {
+      label: {
+        en: "Side",
+      },
+      type: "TextSelect",
+      defaultValue: "dialog",
+      options: {
+        options: [
+          { value: "top", label: { en: "Top" } },
+          { value: "right", label: { en: "Right" } },
+          { value: "bottom", label: { en: "Bottom" } },
+          { value: "left", label: { en: "Left" } },
+        ],
+      },
+      hidden: (content) => {
+        return content.type !== "sheet";
+      },
+    },
+    modalAlignment: {
+      label: {
+        en: "Align",
+      },
+      type: "TextSelect",
+      defaultValue: "dialog",
+      options: {
+        options: [
+          { value: "top", label: { en: "Top" } },
+          { value: "center", label: { en: "Center" } },
+          { value: "bottom", label: { en: "Bottom" } },
+        ],
+      },
+      hidden: (content) => {
+        return content.type !== "modal";
+      },
+    },
+    modalSide: {
+      label: {
+        en: "Side",
+      },
+      type: "TextSelect",
+      defaultValue: "dialog",
+      options: {
+        options: [
+          {
+            value: "left",
+            label: { en: "Left" },
+          },
+          {
+            value: "center",
+            label: { en: "Center" },
+          },
+          { value: "right", label: { en: "Right" } },
+        ],
+      },
+      hidden: (content) => {
+        return content.type !== "modal";
+      },
+    },
+
+    teleport: {
+      label: {
+        en: "Teleport",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+    modal: {
+      label: {
+        en: "Modal",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+
+    manualControl: {
+      label: {
+        en: "Manual control",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+    value: {
+      label: {
+        en: "Value",
+      },
+      hidden: (content) => {
+        return !content.manualControl;
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: true,
+    },
+    trigger: {
+      label: {
+        en: "Trigger",
+      },
+      hidden: (content) => {
+        return content.manualControl;
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+    overlay: {
+      label: {
+        en: "Overlay",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+    escapeClose: {
+      label: {
+        en: "ESC closes",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
     },
   },
 };
